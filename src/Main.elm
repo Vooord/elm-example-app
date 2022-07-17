bf978de4305d2ec5252bfb6c4706b284f80f9790
Yan Novak <yan.novak.2014@gmail.com>
2022-07-17 10:32:17 +0300

    `checkbox` and `input` with no interaction

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,23 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (text)
+import Html exposing (button, div, form, h1, input, label, text)
+import Html.Attributes exposing (checked, class, for, id, placeholder, required, type_, value)
 
 
 type alias Model =
-    {}
+    { email : String
+    , checkbox : Bool
+    }
 
 
 main : Program () Model msg
 main =
     Browser.sandbox
-        { init = {}
+        { init =
+            { email = ""
+            , checkbox = False
+            }
         , update = update
         , view = view
         }
@@ -21,5 +27,14 @@ update msg model =
     model
 
 
+view : Model -> Html.Html msg
 view model =
-    text "hello!"
+    form [ class "form" ]
+        [ h1 [] [ text "Do you wanna work in FAANG?" ]
+        , input [ placeholder "email", value model.email ] []
+        , div []
+            [ input [ id "check", type_ "checkbox", required True, checked model.checkbox ] []
+            , label [ for "check" ] [ text "I agree to sell my soul." ]
+            ]
+        , button [] [ text "Submit" ]
+        ]
